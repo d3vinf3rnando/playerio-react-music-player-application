1d865960a3807da5039fc6805ed2efdf49fc81dc
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-10-03 16:43:36 +0530

    Login screen Buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Login from './Login';
 
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Login/>
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+        <div className='flex justify-end p-10'>
+          <button className='hover:text-purple-500' type="button">Skip</button>
+        </div>
+    </div>
+  )
+}
