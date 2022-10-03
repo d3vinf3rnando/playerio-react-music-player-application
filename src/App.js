8ff796fee7734e3cb230de944b7d38f47918b791
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-10-03 16:17:13 +0530

    main changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from '../Components/Login'
+
 
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      hello world
     </div>
   );
 }
 
 export default App;
+ 
