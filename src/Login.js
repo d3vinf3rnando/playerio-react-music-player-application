6df33846c4492d0ff3ef6644c4951c79716099c0
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-10-04 00:15:12 +0530

    Login page contents

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,35 @@
 import React from 'react'
 
+import Logo from './logo.png'
+
 export default function Login() {
   return (
     <div>
         <div className='flex justify-end p-10'>
-          <button className='hover:text-purple-500' type="button">Skip</button>
+          <button className='hover:text-purple-500 text-lg' type="button">Skip</button>
         </div>
+
+        <div className='flex justify-start'>
+          <div className="bg-purple-200 w-64 h-64 rounded-full blur-3xl "></div>
+        </div>
+
+        <div className=''>
+
+          <div className='flex justify-center z-0'>
+            <div className=" mr-14 bg-purple-200 w-64 h-64 rounded-full blur-3xl z-0 "></div>
+          </div>
+
+          <div className="flex justify-center z-10">
+            <img className='z-10 left-2 z-10' src={Logo} />
+          </div>
+
+        </div>
+
+        <div className='flex justify-end'>
+          <div className="bg-purple-200 w-64 h-64 rounded-full blur-3xl "></div>
+        </div>
+
+        
     </div>
   )
 }
