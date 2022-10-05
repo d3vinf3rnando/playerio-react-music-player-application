2c6f66588e182eee78f528ee1a5b6f4b01a37cd1
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-10-05 08:00:28 +0530

    Route changes

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+
+export default function home() {
+  return (
+    <div>
+        <div className="z-0 relative">
+        <div className='flex justify-end p-10'>
+          <button className='hover:text-purple-500 text-lg' type="button">Skip</button>
+        </div>
+
+        <div className='flex justify-start'>
+          <div className="bg-purple-200 w-64 h-64 rounded-full blur-3xl "></div>
+        </div>
+
+        
+
+          <div className='flex justify-center z-0'>
+            <div className=" mr-14 bg-purple-200 w-64 h-64 rounded-full blur-3xl z-0 "></div>
+          </div>
+ 
+
+        <div className='flex justify-end'>
+          <div className="bg-purple-200 w-64 h-64 rounded-full blur-3xl "></div>
+        </div>
+        {/* updated comments */}
+
+        
+    </div>
+    </div>
+  )
+}
--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,27 @@ import React from 'react'
 
 import Logo from './logo.png'
 
+import { BrowserRouter as Router, Switch, 
+  Route, Redirect,} from "react-router-dom";
+
+  import { Link } from "react-router-dom";
+
+
+  import Home from "./Home";
+
 export default function Login() {
   return (
+
+   
+
+
+
     <div>
+
+    
+  
+
+      <div className="z-0 relative">
         <div className='flex justify-end p-10'>
           <button className='hover:text-purple-500 text-lg' type="button">Skip</button>
         </div>
@@ -13,21 +31,12 @@ export default function Login() {
           <div className="bg-purple-200 w-64 h-64 rounded-full blur-3xl "></div>
         </div>
 
-        <div className=''>
+        
 
           <div className='flex justify-center z-0'>
             <div className=" mr-14 bg-purple-200 w-64 h-64 rounded-full blur-3xl z-0 "></div>
           </div>
-
-          <div className="mx-auto">
-            <img className='z-20  mx-auto max-w-full h-auto mh-10 ' src={Logo} />
-          </div>
-
-          <div className="mx-auto">
-            <img className='z-20  mx-auto max-w-full h-auto mh-10 ' src={Logo} />
-          </div>
-
-        </div>
+ 
 
         <div className='flex justify-end'>
           <div className="bg-purple-200 w-64 h-64 rounded-full blur-3xl "></div>
@@ -35,6 +44,18 @@ export default function Login() {
         {/* updated comments */}
 
         
+    </div>
+
+      <div className=" z-10 my-5  ">
+        <img className=' mx-auto max-w-full h-auto absolute  top-1/3 right-16 left-20 bottom-0  ' src={Logo} />
+      </div>
+
+      <div className="z-20">
+        <h1 className="float-left	text-2xl p-10 absolute  bottom-24  font-bold">Playio.</h1>
+       
+        <p className="flex justify-start mt-20 p-10	absolute  bottom-2 text-lg text-gray-400 text-left">Playio supports artists with tools to create, <br></br>
+          release, and measure music across a global<br></br> stage. </p>
+      </div>
     </div>
   )
 }
