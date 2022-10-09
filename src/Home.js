d983051929c3c302612595a224d40852211169cc
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-10-09 20:59:17 +0530

    changes

--- src/Home.js
+++ src/Home.js
@@ -61,6 +61,11 @@ export default function home() {
             <img className=' absolute top-0 p-5' src={Menu} />
           </a>
         </div>
+        <div className='flex justify-start'>
+          <a href="https://google.com" target="" rel="">
+            <img className=' absolute top-0 p-5' src={Menu} />
+          </a>
+        </div>
         
 
         <div>
